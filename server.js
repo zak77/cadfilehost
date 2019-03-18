512ebb7e20e53c234236eaa2781d86b223d087bd
unknown <zeeshan@chizel.io>
2019-03-18 12:02:21 +0530

    added json

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require("express");
- 
+var fs = require('fs');
 var app = express();
  
 app.use(express.static('models'));
@@ -8,7 +8,20 @@ app.use(express.static('models'));
 // app.use('/css', express.static(__dirname + '/public/css'));
 // app.use('/js', express.static(__dirname + '/public/js'));
 // app.use('/images', express.static(__dirname + '/public/images'));
- 
+app.get('/getfiles', function(req, res){
+    var path = 'models';
+    fs.readdir(path, function(err, items) {
+        console.log(items);
+        var object = {};
+        object.files = items;
+        res.send(object)
+
+        for (var i=0; i<items.length; i++) {
+            console.log(items[i]);
+        }
+    });
+});
+
 var server = app.listen(7080, function(){
     var port = server.address().port;
     console.log("Server started at http://localhost:%s", port);
